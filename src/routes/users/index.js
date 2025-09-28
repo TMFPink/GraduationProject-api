280ba37686a68f28fa062a91abf009836c69dd35
Hong Tran <nguyenhongtranminh@gmail.com>
2025-09-28 19:04:21 +0700

    feat: setup swagger doc

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const { webSpecification } = require('../../configs/Documentation/swagger-config');
+const { appSpecification } = require('../../configs/Documentation/swagger-config');
 
 users_api = express();
-users_api.use('/api-docs', swaggerUi.serveFiles(webSpecification), swaggerUi.setup(webSpecification));
+users_api.use('/api-docs', swaggerUi.serveFiles(appSpecification), swaggerUi.setup(appSpecification));
 users_api.use('/cards', (req, res) => {
     res.json({ message: 'This is the cards endpoint' });
 });
