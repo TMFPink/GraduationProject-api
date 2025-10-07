197dd0bb337dddb7028c07a0d32a28447c7f0cfb
Hong Tran <nguyenhongtranminh@gmail.com>
2025-10-07 21:50:33 +0700

    feat: implement role-based access control and enhance API documentation

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const app = require("./src/app");  
+const app = require('./src/app');
 
 // const PORT = process.env.SERVER_PORT || 3000
 
 const PORT = 3000;
 
-const server = app.listen( PORT, () => {
-    
-    console.log(`Connected: ${PORT}`)
-    console.log(`API docs: http://localhost:${PORT}/v1/api-docs`)
-})
+const server = app.listen(PORT, () => {
+  console.log(`=============Connected: ${PORT}=============`);
+  console.log(
+    `API docs: \nMobileApp: http://localhost:${PORT}/v1/api-docs \nAdmin: http://localhost:${PORT}/v1/admin/api-docs \n`
+  );
+  console.log(`===========================================`);
+});
 
 process.on('SIGINT', () => {
-    server.close( () => console.log(`Exit!`))
-})
+  server.close(() => console.log(`Exit!`));
+});
--- src/routes/users/user.js
+++ src/routes/users/user.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/user.controller');
 const { verifyToken } = require('../../middlewares/auth');
+const { verifyRole } = require('../../middlewares/verifyRole');
 const { asyncHandler } = require('../../helpers/helpers');
 
 router.get('/me', verifyToken, asyncHandler(userController.get_current_user));
-router.get('/', verifyToken, asyncHandler(userController.get_list_users));
+router.get(
+  '/',
+  verifyToken,
+  verifyRole('admin'),
+  asyncHandler(userController.get_list_users)
+);
 router.get('/:id', verifyToken, asyncHandler(userController.get_user_by_id));
 
 module.exports = router;
